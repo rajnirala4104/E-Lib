b20965a7f23a6855df8b05947817ea5df4d9ecb6
Raj Nirala <rajnirala4104@gmail.com>
2023-05-18 00:21:39 +0530

    react added

---
+++ e-library/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import ImageSection from './components/frontPageImage';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <ImageSection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ e-library/src/components/frontPageImage.js
@@ -0,0 +1,24 @@
+function ImageSection(props) {
+    return (
+        <>
+            <div id="carouselExampleSlidesOnly" className="carousel back slide" data-ride="carousel">
+                <div className="webContext">
+                    <div className="webTitle display-4">
+                        E-<span className="liText">Library</span>
+                    </div>
+                    <div className="webTheme">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum voluptatum, iure ut nulla magni sint
+                        aperiam necessitatibus, officiis nostrum voluptatem dolore, quidem officia cumque molestias sed saepe?
+                        Ea, rerum inventore.
+                    </div>
+                    <div className="btns">
+                        <a className="exloreBtn btn btn-warning" href="#booksContainer">Exlore The New World</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ImageSection;
+
