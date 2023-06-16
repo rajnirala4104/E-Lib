05661d5c7978ae894c269e48977a705fc728fccb
Raj Nirala <rajnirala4104@gmail.com>
2023-06-17 01:48:31 +0530

    buys now logic is working but css is not

--- e-library/src/pages/BuyNow.js
+++ e-library/src/pages/BuyNow.js
@@ -1,70 +1,77 @@
 import { useParams } from 'react-router-dom'
 import './css/buyNowPage.css'
-
+import bookData from '../mybooks.json'
 export const BuyNowPage = (props) => {
 
-    const paymentMethod = () => {
-        alert("Oops!! Backend developer ne kuchh galti ki hai")
-    }
+    const paymentMethod = () => alert("Oops!! Backend developer ne kuchh galti ki hai")
     const { bookId } = useParams()
-    console.log(bookId)
-
-    return (
-        <>
-            <div className="buyknow">
-                <div className="container showBookDetailsContainer ">
-                    <div className="bookDetails">
-                        <div className="bookImg"></div>
-                        <div className="bookContent">
-                            <div className="bookName display-4">{"{ Name }"}</div>
-                            <div className="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim dolor fugiat placeat facilis atque temporibus tenetur, quae esse iure neque!</div>
-                            <div className="spacificDetails">
-                                <strong>Author</strong>: {"{ AuthorName }"}<br />
-                                <strong>Publish Data</strong>: {"{ PublicshData }"}<br />
-                                <strong>Type</strong>  : {"{ Type }"}<br />
+    const books = bookData.Books.find(book => book.BookId == bookId.toString())
+    if (!books) {
+        return (
+            <>
+                <div className="container display3">
+                    Oops!!
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <div className="buyknow">
+                    <div className="container showBookDetailsContainer ">
+                        <div className="bookDetails">
+                            <div className="bookImg">
+                                <img src={books.BookImage} alt={books.BookName} />
+                            </div>
+                            <div className="bookContent">
+                                <div className="bookName display-4">{books.BookName}</div>
+                                <div className="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim dolor fugiat placeat facilis atque temporibus tenetur, quae esse iure neque!</div>
+                                <div className="spacificDetails">
+                                    <strong>Author</strong>: {books.BookAuthor}<br />
+                                    <strong>Type</strong>  : {books.BookType}<br />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="paymentMethod">
-                        <div className="paymentContent">
-                            <h2>Payment Details</h2>
-                            <div className="paymentElements">
-                                <div className="form-group mx-sm-3 mb-2">
-                                    <input type="number" className="form-control" id="cardNumber" placeholder="Card Number" />
-                                </div>
-                                <div className="form-group mx-sm-3 mb-2">
-                                    <input type="text" className="form-control" id="cardholdeName" placeholder="Cardholder Name" />
-                                </div>
-                                <div className="cardDateDetails">
-
+                        <div className="paymentMethod">
+                            <div className="paymentContent">
+                                <h2>Payment Details</h2>
+                                <div className="paymentElements">
                                     <div className="form-group mx-sm-3 mb-2">
-                                        <input type="password" className="form-control" id="exampleNum" placeholder="12" />
+                                        <input type="number" className="form-control" id="cardNumber" placeholder="Card Number" />
                                     </div>
-
-
                                     <div className="form-group mx-sm-3 mb-2">
-                                        <input type="password" className="form-control" id="exampleYeat" placeholder="2023" />
+                                        <input type="text" className="form-control" id="cardholdeName" placeholder="Cardholder Name" />
                                     </div>
+                                    <div className="cardDateDetails">
+
+                                        <div className="form-group mx-sm-3 mb-2">
+                                            <input type="password" className="form-control" id="exampleNum" placeholder="12" />
+                                        </div>
 
 
-                                    <div className="form-group mx-sm-3 mb-2">
-                                        <label htmlFor="inputPassword2" className="sr-only">Password</label>
-                                        <input type="password" className="form-control" id="CVV" placeholder="CVV" />
+                                        <div className="form-group mx-sm-3 mb-2">
+                                            <input type="password" className="form-control" id="exampleYeat" placeholder="2023" />
+                                        </div>
+
+
+                                        <div className="form-group mx-sm-3 mb-2">
+                                            <label htmlFor="inputPassword2" className="sr-only">Password</label>
+                                            <input type="password" className="form-control" id="CVV" placeholder="CVV" />
+                                        </div>
+
                                     </div>
-
-                                </div>
-                                <div className="paymentBtns">
-                                    <button className="btn btn-warning mx-2" onClick={paymentMethod}>Confirm and Pay 300₹</button>
-                                    <button className="btn btn-outline-warning">Cancel</button>
+                                    <div className="paymentBtns">
+                                        <button className="btn btn-warning mx-2" onClick={paymentMethod}>Confirm and Pay {books.price}</button>
+                                        <button className="btn btn-outline-warning">Cancel</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            {/* <Footer/> */}
-        </>
-    )
+            </>
+        )
+    }
 }
 
 
