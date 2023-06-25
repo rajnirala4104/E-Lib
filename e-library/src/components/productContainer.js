4d186948ec3a15163fc7d3c8150e0077e51dcc1a
raj nirala <rajnirala4104@gmail.com>
2023-06-25 23:56:04 +0530

    added search button in product container

--- e-library/src/components/productContainer.js
+++ e-library/src/components/productContainer.js
@@ -1,38 +1,39 @@
 import Card from "./BooksCard";
 import Category from "./categorySection";
 import { useState } from "react";
-import bookData from '../mybooks.json'
+import bookData from "../mybooks.json";
 
 function ProductsContainer(props) {
-
-    const [catText, setCatText] = useState("all")
-    const catClickedBtn = (catString) => {
-        setCatText(catString)
-    }
-    return (
-        <>
-            <div className="productsCardContainer" id="booksContainer">
-                <div className="container d-flex justify-content-center display-4">
-                    Books
-                </div>
-                <br />
-                <Category getCat={catClickedBtn} />
-                <div className="productsCardContainer onlyCards">
-                    {bookData.Books.map((bookDic, index) => {
-                        if (bookDic.BookType === catText) {
-                            return (
-                                <Card key={index} {...bookDic}/>
-                            )
-                        } else if (catText === "all") {
-                            return (
-                                <Card key={index} {...bookDic}/>
-                            )
-                        }
-                    })}
-                </div>
-            </div>
-        </>
-    )
+  const [catText, setCatText] = useState("all");
+  const catClickedBtn = (catString) => {
+    setCatText(catString);
+  };
+  return (
+    <>
+      <div className="productsCardContainer" id="booksContainer">
+        <div className="productHeader container d-flex justify-content-center display-4">
+          Books
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+        </div>
+        <br />
+        <Category getCat={catClickedBtn} />
+        <div className="cardContainer onlyCards">
+          {bookData.Books.map((bookDic, index) => {
+            if (bookDic.BookType === catText) {
+              return <Card key={index} {...bookDic} />;
+            } else if(catText === "all") {
+              return <Card key={index} {...bookDic} />;
+            }
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ProductsContainer;
--- e-library/src/shared/components/NavBar.js
+++ e-library/src/shared/components/NavBar.js
@@ -1,45 +1,67 @@
-import { Link } from "react-router-dom" 
+import { Link } from "react-router-dom";
 function NavBar(props) {
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link className="navbar-brand" to="/">E<span className="libText">.Lib</span>
-                </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">
+          E<span className="libText">.Lib</span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/about">About</Link>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Category
-                            </Link>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <button className="dropdown-item" >Fictional Story</button>
-                                <Link className="dropdown-item" >Business</Link>
-                                <Link className="dropdown-item" >Philosophy</Link>
-                                <Link className="dropdown-item" >Self Development</Link>
-                                <Link className="dropdown-item" >Educational</Link>
-                            </div>
-                        </li>
-                    </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input  className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-warning my-2 my-sm-0 searchBtn" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
-        </>
-    )
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link
+                className="nav-link dropdown-toggle"
+                to="/"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Category
+              </Link>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <button className="dropdown-item">Fictional Story</button>
+                <Link className="dropdown-item">Business</Link>
+                <Link className="dropdown-item">Philosophy</Link>
+                <Link className="dropdown-item">Self Development</Link>
+                <Link className="dropdown-item">Educational</Link>
+              </div>
+            </li>
+          </ul>
+          <a
+            className="btn btn-outline-warning my-2 my-sm-0 searchBtn"
+            href="#booksContainer"
+            type="submit"
+          >
+            Search
+          </a>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-
-export default NavBar
-
+export default NavBar;
