99de8e242aa449e01189d42942d39fd18a960f56
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-08-28 21:19:42 +0530

    too itbit changes

--- e-library/src/pages/Home.js
+++ e-library/src/pages/Home.js
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import FrontPageImage from "../components/frontPageImage";
 import ProductsContainer from "../components/productContainer";
 import Footer from "../shared/components/Footer";
@@ -5,10 +6,10 @@ import Footer from "../shared/components/Footer";
 export const Home = (props) => {
    document.title = "E-Lib";
    return (
-      <>
+      <Fragment>
          <FrontPageImage />
          <ProductsContainer />
          <Footer />
-      </>
+      </Fragment>
    );
 };
