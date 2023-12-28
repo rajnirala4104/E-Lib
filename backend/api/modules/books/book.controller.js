dc2688e4ba5a65bdb2cd44f2a625b3c3bc4b965c
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-12-28 14:05:00 +0530

    developed insert and update function

--- backend/api/modules/books/book.controller.js
+++ backend/api/modules/books/book.controller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const { StatusCodes } = require("http-status-codes");
 const { BookModle } = require("./book.model");
+const { LOGGER } = require("../../common/logger");
 const bookControllers = {
    getBooksData: asyncHandler(async (req, res) => {
       try {
@@ -34,9 +35,60 @@ const bookControllers = {
          });
       }
    }),
-   insertBookInTheDatabase: asyncHandler((req, res) => {}),
-   updateBookData: asyncHandler(async (req, res) => {}),
-   deleteBookFromTheDatabase: asyncHandler(async (req, res) => {}),
+   insertBookInTheDatabase: asyncHandler(async (req, res) => {
+      try {
+         const { name, description, author, type, BookImage, price, discountPrice } = req.body;
+         if (!name || !description || !author || !type || !BookImage || !price || !discountPrice) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+               message: "Oops!! bad data given",
+               status: StatusCodes.BAD_REQUEST,
+               data: null
+            })
+         }
+
+         const existBook = await BookModle.findOne({ name, description, BookImage })
+         if (existBook) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+               message: "data is already in our database",
+               data: null
+            })
+         }
+
+         const insertQuary = await BookModle.insertMany({ name, description, author, type, BookImage, price, discountPrice })
+         return res.status(StatusCodes.OK).json({
+            message: "data inserted successfully",
+            status: StatusCodes.OK,
+            data: insertQuary
+         })
+
+      } catch (error) {
+
+         LOGGER.error(`status - ${StatusCodes.INTERNAL_SERVER_ERROR}, Oops something went wrong`)
+         res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+         throw new Error("something went wrong")
+
+      }
+   }),
+   updateBookData: asyncHandler(async (req, res) => {
+      try {
+         const bookId = req.params;
+         const data = req.body;
+         const bookExist = await BookModle.findOne({ _id: bookId.id })
+         if (bookExist) {
+            const updated = await BookModle.findByIdAndUpdate(bookId.id, data)
+            return res.status(StatusCodes.OK).json({
+               message: "book updated successfully ",
+               status: StatusCodes.OK,
+               data: await BookModle.findOne({ _id: bookId.id })
+            })
+         }
+      } catch (error) {
+         res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+         LOGGER.error(`status - ${StatusCodes.INTERNAL_SERVER_ERROR}, - can't update data`)
+         throw new Error(`status - ${StatusCodes.INTERNAL_SERVER_ERROR}, - ${error}`)
+      }
+   }),
+   deleteBookFromTheDatabase: asyncHandler(async (req, res) => { }),
 };
 
 module.exports = { bookControllers };
