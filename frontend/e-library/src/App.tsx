1353db11621c4fb24b84355f23ddd8dabd36bd24
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-08-23 22:21:16 +0530

    chore: setup frontend

---
+++ frontend/e-library/src/App.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import "./App.css";
+function App() {
+  return (
+    <Fragment>
+      <div>Hello world</div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ frontend/e-library/src/pages/Root.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+export const Root: React.FC = () => {
+  return <Outlet />;
+};
