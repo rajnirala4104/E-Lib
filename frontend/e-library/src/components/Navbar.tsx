23d17f40c922b9184a1cb7501d360127c709e24a
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-08-28 15:07:05 +0530

    worked on responsivness and lil bit more changes

--- frontend/e-library/src/components/Navbar.tsx
+++ frontend/e-library/src/components/Navbar.tsx
@@ -7,32 +7,34 @@ const Navbar: React.FC = () => {
 
   return (
     <section className="flex flex-col justify-evenly items-center">
-      <div className="mainNavbar py-5 w-full shadow-md h-[5.6rem] grid grid-cols-4 gap-4 place-content-center">
-        <div className="logo place-content-center text-3xl px-6">
+      <div className="mainNavbar py-5 w-full shadow-md h-[4rem] lg:h-[6rem] grid grid-cols-4 gap-4 place-content-center">
+        <div className="logo place-content-center text-4xl px-6">
           <span>e</span>
           <span className="text-cyan-600 font-bold">.Lib</span>
         </div>
         <div className="searchBar col-span-2 place-content-center h-[3rem]">
           <div
-            className={`grid  grid-cols-9 gap-2 border-2 ${
+            className={`border-2 hidden lg:grid grid-cols-9 place-content-center place-items-center ${
               toggleSearchBarBorder ? " border-[rgba(44,119,151,0.89)]" : ""
-            } place-content-center h-full rounded-full py-3 pl-4 shadow-lg hover:borde-2  hover:border-[rgba(44,119,151,0.89)]`}
+            } hover:borde-2 col-span-8 outline-none text-[rgb(11,42,55)] shadow-lg hover:borde-2  hover:border-[rgba(44,119,151,0.89)] rounded-full border`}
           >
             <input
               onClick={() => setToggleSearchBarBorder(!toggleSearchBarBorder)}
               onBlur={() => setToggleSearchBarBorder(!toggleSearchBarBorder)}
               type="text"
-              placeholder="Search.."
-              className="col-span-8 outline-none boder border-red-500 text-[rgb(11,42,55)]"
+              placeholder="Search..."
+              className={`w-full place-content-center pl-2 ml-3 col-span-8 outline-none text-[rgb(11,42,55)] h-[2rem] rounded-full`}
             />
-            <SearchIcon classes="text-2xl" />
+            <div className="bg-cyan-900 grid place-content-center place-items-center rounded-full h-[2.3rem] w-[2.3rem] my-2 hover:bg-cyan-950 cursor-pointer">
+              <SearchIcon classes="text-xl text-white" />
+            </div>
           </div>
         </div>
-        <div className="loginBtn place-content-center place-items-center grid grid-cols-5 w-[40%] mx-auto ">
-          <span className="col-span-3 text-center text-[18px] font-semibold cursor-pointer py-1 px-4 bg-cyan-300 text-slate-700 shadow-lg hover:shadow-md hover:text-black rounded-full">
+        <div className="loginBtn place-content-center place-items-center j grid lg:grid-cols-5 w-[40%] mx-auto ">
+          <span className="hidden lg:grid col-span-3 text-center text-[18px] font-semibold cursor-pointer py-2 px-4 bg-cyan-300 text-slate-700 shadow-lg hover:shadow-md hover:text-black rounded-full">
             Login
           </span>
-          <UserIcon classes="text-3xl ml-5 shadow-lg rounded-full" />
+          <UserIcon classes="text-3xl lg:text-4xl ml-5 shadow-lg rounded-full cursor-pointer" />
         </div>
       </div>
       <div className="categories w-[100%] flex justify-evenly items-center my-3">
