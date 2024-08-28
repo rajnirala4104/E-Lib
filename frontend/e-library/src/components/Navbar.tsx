c4a65a631fb1d1e834897e104f0dcd6d47072b0f
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-08-28 14:01:32 +0530

    mainNavbar is done

--- frontend/e-library/src/components/Navbar.tsx
+++ frontend/e-library/src/components/Navbar.tsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchIcon, UserIcon } from "../icons";
 
 const Navbar: React.FC = () => {
+  const [toggleSearchBarBorder, setToggleSearchBarBorder] =
+    useState<boolean>(false);
+
   return (
     <section className="flex flex-col justify-evenly items-center">
-      <div className="manNavbar py-5 w-full border border-black grid grid-cols-4 gap-4 place-content-center">
+      <div className="mainNavbar py-5 w-full shadow-md h-[5.6rem] grid grid-cols-4 gap-4 place-content-center">
         <div className="logo place-content-center text-3xl px-6">
-          <span>E</span>
-          <span className="text-red-500 font-bold">.Lib</span>
+          <span>e</span>
+          <span className="text-cyan-600 font-bold">.Lib</span>
         </div>
-        <div className="searchBar col-span-2 place-content-center">
-          <div className="grid grid-cols-9 gap-2 border border-[rgba(0,0,0,0.32)] place-content-center h-full rounded-full p-3">
-            <input type="text" placeholder="Search.." className="col-span-8 " />
-            <SearchIcon classes="text-2xl " />
+        <div className="searchBar col-span-2 place-content-center h-[3rem]">
+          <div
+            className={`grid  grid-cols-9 gap-2 border-2 ${
+              toggleSearchBarBorder ? " border-[rgba(44,119,151,0.89)]" : ""
+            } place-content-center h-full rounded-full py-3 pl-4 shadow-lg hover:borde-2  hover:border-[rgba(44,119,151,0.89)]`}
+          >
+            <input
+              onClick={() => setToggleSearchBarBorder(!toggleSearchBarBorder)}
+              onBlur={() => setToggleSearchBarBorder(!toggleSearchBarBorder)}
+              type="text"
+              placeholder="Search.."
+              className="col-span-8 outline-none boder border-red-500 text-[rgb(11,42,55)]"
+            />
+            <SearchIcon classes="text-2xl" />
           </div>
         </div>
-        <div className="loginBtn">
-          <span>Login</span>
-          <UserIcon />
+        <div className="loginBtn place-content-center place-items-center grid grid-cols-5 w-[40%] mx-auto ">
+          <span className="col-span-3 text-center text-[18px] font-semibold cursor-pointer py-1 px-4 bg-cyan-300 text-slate-700 shadow-lg hover:shadow-md hover:text-black rounded-full">
+            Login
+          </span>
+          <UserIcon classes="text-3xl ml-5 shadow-lg rounded-full" />
         </div>
       </div>
-      <div className="categories border border-blue-500 w-[100%] flex justify-evenly items-center">
+      <div className="categories w-[100%] flex justify-evenly items-center my-3">
         Categories
       </div>
     </section>
