0c53825261c25b286dd4ef8ae884522cc1fe4710
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-08-31 23:23:18 +0530

    feat: build admin registration and login controllers

--- backend/api/modules/admin/admin.controller.js
+++ backend/api/modules/admin/admin.controller.js
@@ -11,9 +11,15 @@ const adminControllers = {
          throw new ApiError(StatusCodes.NOT_FOUND, "password is necessary");
       }
 
-      const admin = await Admin.create({ password }, { new: true });
+      const adminDoesExist = await Admin.findOne({ name: "root" });
+      if (adminDoesExist) {
+         console.log(adminDoesExist);
+         throw new ApiError(StatusCodes.CONFLICT, "Admin is already exist");
+      }
+
+      const admin = await Admin.create({ password });
       return res
-         .Status(StatusCodes.OK)
+         .status(StatusCodes.OK)
          .json(
             new ApiResponse(
                StatusCodes.CREATED,
@@ -22,7 +28,26 @@ const adminControllers = {
             ),
          );
    }),
-   login: asyncHandler(async (req, res) => {}),
+   login: asyncHandler(async (req, res) => {
+      const { name, password } = req.body;
+      if (!name || !password) {
+         throw new ApiError(StatusCodes.NOT_FOUND, "all fields are necessary");
+      }
+
+      const admin = await Admin.findOne({ name });
+      if (!admin) {
+         throw new ApiError(StatusCodes.NOT_FOUND, "admin doesn't exist");
+      }
+
+      const isPasswordTrue = await admin.isPasswordTrue(password);
+      if (!isPasswordTrue) {
+         throw new ApiError(StatusCodes.CONFLICT, "password is wrong");
+      }
+
+      return res
+         .status(StatusCodes.OK)
+         .json(new ApiResponse(StatusCodes.OK, admin, "login successfully"));
+   }),
    logout: asyncHandler(async (req, res) => {}),
 };
 
