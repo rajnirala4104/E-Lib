2e30b54a777c9dadac1422dc7066d94e5c05de11
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-09-02 11:18:59 +0530

    something

--- backend/api/constants.js
+++ backend/api/constants.js
@@ -1,5 +1,7 @@
 const DATABASE_NAME = "YTVP_DB";
-const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
+const CORS_ORIGIN =
+   process.env.CORS_ORIGIN ||
+   "http://localhost:3000https://e-lib-api-eh8c.onrender.com";
 const EXPRESS_RATE_LIMIT = "45kb";
 
 module.exports = { CORS_ORIGIN, DATABASE_NAME, EXPRESS_RATE_LIMIT };
