f11202a279a607ecd64aa5810b275d58612b465b
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-09-02 12:39:35 +0530

    lil bit changes

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,8 @@ require("dotenv").config();
 const { createServer } = require("http");
 const { app } = require("./api/app");
 const { LOGGER } = require("./api/common/logger");
-const connectDatabase = require("./api/database/db.connection");
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 createServer(app).listen(PORT, () => {
    LOGGER.info(`API is running.. [:${PORT}/api/v1/books]`.yellow.bold);
