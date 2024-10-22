f07a9e83022938ebee7fc5148502e22d250effc5
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-10-22 12:06:49 +0530

    feat: ✅ build hero section

--- frontend/e-library/src/components/HeroSection.tsx
+++ frontend/e-library/src/components/HeroSection.tsx
@@ -4,24 +4,29 @@ import { allImages } from "../assets";
 const HeroSection: React.FC = () => {
   return (
     <section
-      className="h-screen w-full flex justify-center items-center"
+      className="h-screen w-full flex justify-center items-center text-white"
       style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(${allImages.heroBg}) center/cover`,
+        background: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('${allImages.heroBg}') center center/cover`,
         backgroundAttachment: "fixed",
       }}
     >
       <div className="w-[60%] h-[50%] flex flex-col justify-center items-center">
         <div className="text-5xl font-bold">
           <span>
-            e-<span className="text-cyan-600 font-serif">library</span>
+            e-<span className="text-cyan-400 font-serif">Library</span>
           </span>
         </div>
-        <div>
+        <div className="w-[60%] my-3">
           <p className="text-center">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sequi
             officia magnam ipsa quasi. Libero nesciunt labore veritatis tempore
             dicta vero, praesentium exercitationem,
           </p>
+        </div>
+        <div>
+          <button className="bg-cyan-600 px-4 py-2 rounded-lg hover:bg-cyan-700 transistion duration-300 ease-in-out">
+            Explore The New World
+          </button>
         </div>
       </div>
     </section>
