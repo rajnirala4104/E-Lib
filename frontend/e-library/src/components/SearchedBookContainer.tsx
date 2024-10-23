a39d71c566692620d648218f0e46d1df74eeda01
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-10-23 08:38:23 +0530

    refactor: add some css in search box

--- frontend/e-library/src/components/SearchedBookContainer.tsx
+++ frontend/e-library/src/components/SearchedBookContainer.tsx
@@ -3,7 +3,7 @@ import { SearchInputValue } from '../types'
 
 const SearchedBookContainer: React.FC<SearchInputValue> = (props) => {
     return (
-        <div className="w-[50%] h-[30rem] rounded-lg my-3 flex justify-center items-center bg-white absolute top-0">
+        <div className="w-[50%] h-[30rem] rounded-lg my-3 flex justify-center items-center bg-blue-50 absolute top-0 shadow-2xl">
             <div>
                 {props.inputValue}
             </div>
